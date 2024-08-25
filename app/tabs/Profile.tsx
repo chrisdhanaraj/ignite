234fde6c553ec76a263cfe4d1c03135993b940ed
Chris Dhanaraj <cdhanaraj@netflix.com>
2024-08-25 12:33:01 -0700

    sketch

--- app/tabs/Profile.tsx
+++ app/tabs/Profile.tsx
@@ -1,5 +1,5 @@
 import { ThemedText } from "@/components/ThemedText";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ProfileScreen() {
